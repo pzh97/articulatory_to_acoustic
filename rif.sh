4ff9cdaeaab50ecc144eda053efa4176fd3816e9
Zihang Peng <zihangpeng@Zihangs-MacBook-Air.local>
2022-12-30 22:26:11 +0000

    command line tool

--- rif.sh
+++ rif.sh
@@ -1,20 +1,19 @@
 #!/bin/sh
 mkdir ./wav
-cp -r ./mocha_timit/msak0_v1.1/*.wav ./wav
+cp -r ./mocha_timit/$1_v1.1/*.wav ./wav
 echo "copying finished!"
 for i in $(seq -f "%03g" 1 460)
 do
-    sph2pipe -f rif ./wav/msak0_$i.wav ./wav/test_$i.wav
+    sph2pipe -f rif ./wav/$1_$i.wav ./wav/test_$i.wav
 done
 echo "The SPH wav files have already been converted to RIF wav files"
-find ./wav -name 'msak0_*.wav' -delete
+find ./wav -name '$1_*.wav' -delete
 for file in ./wav/test_*.wav
 do
-    mv "$file" "${file/test_/msak0_}"
+    mv "$file" "${file/test_/$1_}"
 done
 echo "renaming finished!"
 cp -r ./wav/*.wav ./corpus
 echo "corpus built!"
 rm -r ./wav
 echo "cleaning finished!"
-
