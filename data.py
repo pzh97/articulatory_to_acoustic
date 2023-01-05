b8e33120feb8358faff3a0491344efabb7b587bc
Zihang Peng <zihangpeng@Zihangs-MacBook-Air.local>
2023-01-05 17:51:59 +0000

    voicing added

--- data.py
+++ data.py
@@ -6,6 +6,10 @@ import re
 import sys
 import textgrids
 import os
+import spychhiker
+import matplotlib.pyplot as plt
+import parselmouth
+from itertools import groupby
 np.set_printoptions(threshold=sys.maxsize)
 
 script = []
@@ -21,14 +25,27 @@ for t in script:
         phone.append(l)
 phone_set = set(phone)
 phone_list = list(phone_set)
-ipa = 'a aj aw aː b bʲ c cʰ d dʒ dʲ e ej f fʲ h i iː j k kʰ l m mʲ m̩ n n̩ o ow p pʰ pʲ s t tʃ tʰ tʲ u uː v vʲ w z æ ç ð ŋ ɐ ɑ ɑː ɒ ɒː ɔ ɔj ə əw ɚ ɛ ɛː ɜ ɜː ɝ ɟ ɡ ɪ ɫ ɫ̩ ɱ ɲ ɹ ɾ ʃ ʉ ʉː ʊ ʎ ʒ ʔ θ'
+ipa = 'a aj aw aː b bʲ c cʰ d dʒ dʲ e ej f fʲ h i iː j k kʰ l m mʲ m̩ n n̩ o ow p pʰ pʲ s t tʃ tʰ tʲ u uː v vʲ w z æ ç ð ŋ ɐ ɑ ɑː ɒ ɒː ɔ ɔj ə əw ɚ ɛ ɛː ɜ ɜː ɝ ɟ ɡ ɪ ɫ ɫ̩ ɱ ɲ ɹ ɾ ʃ ʉ ʉː ʊ ʎ ʒ ʔ θ d̪ t̪'
 IPA_list = list(ipa.split(" "))
-#print(list(phone_set - set(IPA_list)))
+print(list(phone_set - set(IPA_list)))
 
 for i in range(len(phone_set)):
     phone_value[phone_list[i]] = i
 
-def data_reader(filename, ema, segmentation):
+def signal2sound(y, sr):    
+    sound = parselmouth.Sound(y)
+    sound.sampling_frequency = sr
+    return sound
+
+def praat_get_pitch(sound, f0min=70, f0max=500):
+
+    f0Obj = sound.to_pitch(pitch_floor=f0min, pitch_ceiling=f0max)
+    f0 = f0Obj.selected_array['frequency']
+    tf0 = f0Obj.xs()    
+    return f0, tf0
+
+
+def data_reader(filename, ema, segmentation, laryngograph):
     with open(filename + '/mocha_timit/msak0_v1.1/' + ema, 'rb') as f:
         file_read = f.read()
     length_header = 0
@@ -55,6 +72,7 @@ def data_reader(filename, ema, segmentation):
     
     time_vec = mtx[:, 0]
     print(time_vec)
+    print(len(time_vec))
 
     phon_max = []
     count = 0
@@ -85,9 +103,36 @@ def data_reader(filename, ema, segmentation):
 
     for n in range(len(time_vec)-index[-1]+1):
         output_array[index[-1]:] = phone_value[labels[-1]]
-    return mtx, output_array
 
-#data_reader('.', 'msak0_268.ema', 'msak0_268.TextGrid')
+    y, sr = librosa.load(filename + '/mocha_timit/msak0_v1.1/' + laryngograph, sr=1500)
+    tlar = np.arange(len(y))/sr
+    print(len(y))
+    print(sr)
+
+    f0, tf0 = praat_get_pitch(signal2sound(y, sr))
+    f0[f0 > 0] = 1
+
+    seqs = [(key, len(list(val))) for key, val in groupby(f0)]
+    seqs = [(key, sum(s[1] for s in seqs[:i]), len) for i, (key, len) in enumerate(seqs)]
+    time_index = [[s[1], s[1]+s[2]-1] for s in seqs if s[0]==1]
+    print(time_index)
+    time_point = []
+    for i in range(len(time_index)):
+        for time in time_index[i]:
+            time_point.append(tf0[time])
+    print(time_point)
+    voicing_array = np.zeros(len(time_vec))
+    voice_index = []
+    for i in range(len(time_point)):
+        voice_result = next(k for k, value in enumerate(time_vec) if time_point[i]<value)
+        voice_index.append(voice_result)
+    print(voice_index)
+    for i in range(0, (len(voice_index)-2), 2):
+        voicing_array[voice_index[i]:voice_index[i+1]]=1
+    print(voicing_array)
+    return mtx, output_array, voicing_array
+
+data_reader('.', 'msak0_260.ema', 'msak0_260.TextGrid', 'msak0_260.lar')
 
 
 
