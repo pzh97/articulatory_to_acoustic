eeabb299b405ec3705cc5ac6d7e5d324d7b4cd6d
Zihang Peng <zihangpeng@Zihangs-MacBook-Air.local>
2023-01-07 21:44:59 +0000

    finally, matrices built
    exit()
    echo echo Hello

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from data import *
 import os
 
-ema_path = './mocha_timit/msak0_v1.1'
+path = './mocha_timit/msak0_v1.1'
 ema_files = []
 lar_files = []
-for data_file in sorted(os.listdir(ema_path)):
+for data_file in sorted(os.listdir(path)):
     if data_file.endswith('.ema'):
         ema_files.append(data_file)
     if data_file.endswith('.lar'):
@@ -17,9 +17,18 @@ error = 0
 zipped = zip(ema_files, transcription, lar_files)
 zipped_list = list(zipped)
 #print(zipped_list)
-error = 0
+#row_length = 0
+articulatory_data = []
+phonetic_data = []
 for f, s, l in zipped_list:
-    error+=1
-    data_reader('.', f, s, l)
-print(error)
-
+    articulatory, phonetic, time_vec = data_reader('.', f, s, l)
+    #row_length += len(time_vec)
+    articulatory_data.append(articulatory)
+    phonetic_data.append(phonetic)
+#print(length)
+a = np.vstack(articulatory_data)
+p = np.vstack(phonetic_data)
+#print(a.shape)
+#(1654242, 23)
+#print(p.shape)
+#(1654242, 1)
