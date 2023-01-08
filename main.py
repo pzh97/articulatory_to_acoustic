7075fadfceade7d2c5ed92d605d6875db676de5a
Zihang Peng <zihangpeng@Zihangs-MacBook-Air.local>
2023-01-08 17:20:13 +0000

    training test

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from data import *
 import os
+import time
+import torch
+from torch.autograd import Variable
 
+tic = time.time()
 path = './mocha_timit/msak0_v1.1'
 ema_files = []
 lar_files = []
@@ -13,7 +17,7 @@ transcription= []
 for grid in sorted(os.listdir('./corpus/textgrids')):
     if grid.endswith('.TextGrid'):
         transcription.append(grid)
-error = 0
+#print(transcription)
 zipped = zip(ema_files, transcription, lar_files)
 zipped_list = list(zipped)
 #print(zipped_list)
@@ -29,3 +33,28 @@ p = np.vstack(phonetic_data)
 #(1654242, 23)
 #print(p.shape)
 #(1654242, 1)
+#print(str(time.time() - tic) + ' s')
+#68.12165307998657 s
+print("start!")
+epochs = 400
+dtype = torch.FloatTensor
+articulatory_d = Variable(torch.from_numpy(a).type(dtype), requires_grad = False)
+phonetic_d = Variable(torch.from_numpy(p).type(dtype), requires_grad = False)
+
+model = torch.nn.Sequential(
+    torch.nn.Linear(23, 100),
+    torch.nn.ReLU(),
+    torch.nn.Linear(100, 1654242),
+    torch.nn.ReLU(),
+    )
+loss_fn = torch.nn.MSELoss()
+for ix in range(epochs):
+    p_hat = model(articulatory_d)
+    loss_var = 0.5 * loss_fn(p_hat, phonetic_d)
+    loss[ix] = loss_var.item()
+
+    model.zero_grad()
+    loss_var.backward()
+    for param in model.parameters():
+        param.data = param.data - param.grad.data
+print(str(time.time() - tic) + ' s')
