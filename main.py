140c13e8c4fc62268eda80ba78cc8ee325598382
Zihang Peng <zihangpeng@Zihangs-MacBook-Air.local>
2023-01-29 19:21:03 +0000

    normalised

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import time
 import torch
 from torch.autograd import Variable
+import torchvision
 
 tic = time.time()
 path = './mocha_timit/msak0_v1.1'
@@ -35,40 +36,12 @@ p = np.vstack(phonetic_data)
 #(1654242, 1)
 #print(str(time.time() - tic) + ' s')
 #68.12165307998657 s
-print("start!")
-epochs = 400
-dtype = torch.FloatTensor
-articulatory_d = Variable(torch.from_numpy(a).type(dtype), requires_grad = False)
-phonetic_d = Variable(torch.from_numpy(p).type(dtype), requires_grad = False)
+m = np.c_[a, p]
+#print(m.shape)
+t = m[:, 2:(m.shape[1])]
+timit = torch.from_numpy(t)
+t_input = timit[:, : (timit.shape[1]-1)]
+t_output = timit[:, (timit.shape[1]-1)]
+mean, std = torch.mean(t_input, 0), torch.std(t_input, 0)
+normalised_input = torchvision.transforms.Normalize(mean, std)
 
-model = torch.nn.Sequential(
-    torch.nn.Linear(23, 100),
-    torch.nn.ReLU(),
-    torch.nn.Linear(100, 1654242),
-    torch.nn.ReLU(),
-    )
-loss_fn = torch.nn.MSELoss()
-for ix in range(epochs):
-    p_hat = model(articulatory_d)
-    loss_var = 0.5 * loss_fn(p_hat, phonetic_d)
-    loss[ix] = loss_var.item()
-
-    model.zero_grad()
-    loss_var.backward()
-    for param in model.parameters():
-        param.data = param.data - param.grad.data
-print(str(time.time() - tic) + ' s')
-
-"""
-# Ploting loss vs epochs
-plt.figure()
-ix = np.arange(epochs)
-plt.plot(ix, loss)
-
-# Training Accuracy
-p_hat = model(X)
-p_tmp = torch.max(p_hat, dim=1)[1]
-phat = phone_value[p_tmp.data.numpy()]
-acc = np.mean(1 * (phat == phonetic_d))
-print('Training Accuracy: ' + str(acc*100))
-"""
