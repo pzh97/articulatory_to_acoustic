1ace30c51ed84198d779ea050bf8be2b41cfa0f4
Zihang Peng <zihangpeng@Zihangs-MacBook-Air.local>
2023-02-04 15:02:03 +0000

    preliminary_model

--- model.py
+++ model.py
@@ -1,18 +1,34 @@
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
+import numpy as np
 
-class Net(torch.nn.Module):
-    def __init__(self, n_feature, n_hidden, n_output):
-        super(Net, self).__init__()
-        self.hidden = torch.nn.Linear(n_feature, n_hidden)  
-        self.out = torch.nn.Linear(n_hidden, n_output)  
+input_dim = 15
+hidden_layers = 100
+output_dim = 43
+
+class Network(nn.Module):
+    def __init__(self):
+        super(Network, self).__init__()
+        self.linear1 = nn.Linear(input_dim, hidden_layers)
+        self.linear2 = nn.Linear(hidden_layers, output_dim)
 
     def forward(self, x):
-        x = F.relu(self.hidden(x))     
-        x = self.out(x)
+        x = torch.sigmoid(self.linear1(x))
+        x = self.linear2(x)
         return x
 
-net = Net(n_feature=22, n_hidden=100, n_output=81)
-net = net.float()
+class Data(Dataset):
+    def __init__(self, X_train, y_train):
+        self.X = torch.from_numpy(X_train.astype(np.float32))
+        self.y = torch.from_numpy(y_train).type(torch.LongTensor)
+        self.len = self.X.shape[0]
+
+    def __getitem__(self, index):
+        return self.X[index], self.y[index]
+
+    def __len__(self):
+        return self.len
+
 
