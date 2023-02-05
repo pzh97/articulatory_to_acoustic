1127d44befb1abfca665de553410a4a68f3754db
Zihang Peng <zihangpeng@Zihangs-MacBook-Air.local>
2023-02-05 22:13:31 +0000

    softmax

--- data.py
+++ data.py
@@ -7,7 +7,6 @@ import sys
 import textgrids
 import os
 import spychhiker
-import matplotlib.pyplot as plt
 import parselmouth
 from itertools import groupby
 np.set_printoptions(threshold=sys.maxsize)
@@ -15,21 +14,16 @@ np.set_printoptions(threshold=sys.maxsize)
 script = []
 phone = []
 phone_value = {}
-for transcription in sorted(os.listdir('./corpus/textgrids')):
+for transcription in sorted(os.listdir('./female_us/textgrids')):
     if transcription.endswith('.TextGrid'):
         script.append(transcription)
 for t in script:
-    grid = textgrids.TextGrid('./corpus/textgrids/' + t)
+    grid = textgrids.TextGrid('./female_us/textgrids/' + t)
     for phon in grid['phones']:
         l = phon.text.transcode()
         phone.append(l)
 phone_set = set(phone)
 phone_list = list(phone_set)
-#ipa = 'a aj aw aː b bʲ c cʰ d dʒ dʲ e ej f fʲ h i iː j k kʰ l m mʲ m̩ n n̩ o ow p pʰ pʲ s t tʃ tʰ tʲ u uː v vʲ w z æ ç ð ŋ ɐ ɑ ɑː ɒ ɒː ɔ ɔj ə əw ɚ ɛ ɛː ɜ ɜː ɝ ɟ ɡ ɪ ɫ ɫ̩ ɱ ɲ ɹ ɾ ʃ ʉ ʉː ʊ ʎ ʒ ʔ θ d̪ t̪'
-#IPA_list = list(ipa.split(" "))
-#print(list(phone_set - set(IPA_list)))
-print(len(phone_list))
-print(phone_list)
 
 for i in range(len(phone_set)):
     phone_value[phone_list[i]] = i
@@ -47,7 +41,7 @@ def praat_get_pitch(sound, f0min=70, f0max=500):
 
 
 def data_reader(filename, ema, segmentation, laryngograph):
-    with open(filename + '/mocha_timit/msak0_v1.1/' + ema, 'rb') as f:
+    with open(filename + '/fsew0_v1.1/' + ema, 'rb') as f:
         file_read = f.read()
     length_header = 0
     isContinue = True
@@ -59,13 +53,6 @@ def data_reader(filename, ema, segmentation, laryngograph):
             length_header +=1
         
     data_bytes = file_read[length_header:]
-    #data_header = file_read[:(length_header+1)]
-    #print(data_header)
-    """
-    b'EST_File Track\nDataType binary\nNumFrames 1548\nByteOrder 01\nNumChannels 20\nEqualSpace 1\nBreaksPresent true\nCommentChar ;\n\nChannel_0 ui_x\nChannel_1 li_x\nChannel_2 ul_x\nChannel_3 ll_x\nChannel_4 tt_x\nChannel_5 ui_y\nChannel_6 li_y\nChannel_7 ul_y\nChannel_8 ll_y\nChannel_9 tt_y\nChannel_10 tb_x\nChannel_11 td_x\nChannel_12 v_x \nChannel_13 ****\nChannel_14 bn_x\nChannel_15 tb_y\nChannel_16 td_y\nChannel_17 v_y \nChannel_18 ****\nChannel_19 bn_y\nEST_Header_End\n
-    sensor_we_need = ["li", "ul", "ll", "tt", "tb", "td", "v"]
-    sensor_need_idx = [3, 4, 5, 6, 12, 13, 14]
-    """
  
     nb_bytes = len(data_bytes)
     nb_v = int(nb_bytes/4)
@@ -80,15 +67,12 @@ def data_reader(filename, ema, segmentation, laryngograph):
         mtx[n, :] = data[n*nb_col:(n+1)*nb_col]
     
     time_vec = mtx[:, 0]
-    #print(mtx.shape)
-    #print(time_vec)
-    #print(len(time_vec))
 
     phon_max = []
     count = 0
     labels = []
 
-    grid = textgrids.TextGrid(filename + '/corpus/textgrids/' + segmentation)
+    grid = textgrids.TextGrid(filename + '/female_us/textgrids/' + segmentation)
 
     for phon in grid['phones']:
         count+=1
@@ -116,10 +100,8 @@ def data_reader(filename, ema, segmentation, laryngograph):
 
     output_array = output_array.reshape(-1, 1)
 
-    y, sr = librosa.load(filename + '/mocha_timit/msak0_v1.1/' + laryngograph, sr=1500)
+    y, sr = librosa.load(filename + '/fsew0_v1.1/' + laryngograph, sr=1500)
     tlar = np.arange(len(y))/sr
-    #print(len(y))
-    #print(sr)
 
     f0, tf0 = praat_get_pitch(signal2sound(y, sr))
     f0[f0 > 0] = 1
@@ -127,26 +109,21 @@ def data_reader(filename, ema, segmentation, laryngograph):
     seqs = [(key, len(list(val))) for key, val in groupby(f0)]
     seqs = [(key, sum(s[1] for s in seqs[:i]), len) for i, (key, len) in enumerate(seqs)]
     time_index = [[s[1], s[1]+s[2]-1] for s in seqs if s[0]==1]
-    #print(time_index)
     time_point = []
     for i in range(len(time_index)):
         for time in time_index[i]:
             time_point.append(tf0[time])
     time_point = [x for x in time_point if x<=time_vec[-1]]
-    #print(time_point)
     voicing_array = np.zeros(len(time_vec))
     voice_index = []
     for i in range(len(time_point)):
         voice_result = next(k for k, value in enumerate(time_vec) if time_point[i]<value)
         voice_index.append(voice_result)
-    #print(voice_index)
     for i in range(0, (len(voice_index)-2), 2):
         voicing_array[voice_index[i]:voice_index[i+1]]=1
     conca = np.c_[mtx, voicing_array]
-    #print(conca.shape)
     return conca, output_array, time_vec
 
-#data_reader('.', 'fsew0_001.ema', 'fsew0_001.TextGrid', 'fsew0_001.lar')
 
 
 
--- main.py
+++ main.py
@@ -2,12 +2,13 @@ from data import *
 import os
 import time
 import torch
+import torch.nn.functional as F
 from torch.autograd import Variable
 from model import *
 
 if __name__ == '__main__':
     tic = time.time()
-    path = './mocha_timit/msak0_v1.1'
+    path = './fsew0_v1.1'
     ema_files = []
     lar_files = []
     for data_file in sorted(os.listdir(path)):
@@ -16,13 +17,11 @@ if __name__ == '__main__':
         if data_file.endswith('.lar'):
             lar_files.append(data_file)
     transcription= []
-    for grid in sorted(os.listdir('./corpus/textgrids')):
+    for grid in sorted(os.listdir('./female_us/textgrids')):
         if grid.endswith('.TextGrid'):
             transcription.append(grid)
-    #print(transcription)
     zipped = zip(ema_files, transcription, lar_files)
     zipped_list = list(zipped)
-    #print(zipped_list)
     articulatory_data = []
     phonetic_data = []
     for f, s, l in zipped_list:
@@ -31,28 +30,18 @@ if __name__ == '__main__':
         phonetic_data.append(phonetic)
     a = np.vstack(articulatory_data)
     p = np.vstack(phonetic_data)
-    #print(a.shape)
-    #(1654242, 23)
-    #print(p.shape)
-    #(1654242, 1)
-    #68.12165307998657 s
+    #female_speaker=(834215, 23)
     m = np.c_[a, p]
-    #m.shape=(1654242, 24)including labels
     indices = np.random.permutation(m.shape[0])
     train_size = int(0.8*(m.shape[0]))
     training_idx, test_idx = indices[:train_size], indices[train_size:]
     training, testing = m[training_idx, :], m[test_idx, :]
-    #print(training.shape)=(1323393, 24)
-    #print(test.shape)=(330849, 24)
     idx_in_columns = [3, 4, 5, 6, 8, 9, 10, 11, 12, 13, 14, 17, 18, 19, 22]
     train = training[:, idx_in_columns]
     labels_training = training[:, (training.shape[1]-1)]
-    #labels_training = labels_training[:, np.newaxis]
-    #print(labels_training.shape)=(1323393, 1)
     train_mean = np.mean(train, axis=0)
     train_std = np.std(train, axis=0)
     train_zscore = (train-train_mean)/train_std
-    #print(time.time()-tic)
     traindata = Data(train_zscore, labels_training)
     batch_size = 1000
     trainloader = DataLoader(traindata, batch_size=batch_size, shuffle=True, num_workers=2)
@@ -82,18 +71,26 @@ if __name__ == '__main__':
     testdata = Data(test_zscore, labels_testing)
     testloader = DataLoader(testdata, batch_size=batch_size, shuffle=True, num_workers=2)
 
-    network = Network()
     network.load_state_dict(torch.load('./model.pth'))
 
-    dataiter = iter(testloader)
-    inputs, labels = next(dataiter)
     correct, total = 0, 0
     with torch.no_grad():
         for data in testloader:
             inputs, labels = data
             outputs = network(inputs)
             __, predicted = torch.max(outputs.data, 1)
+            #prob = F.softmax(outputs, dim=1)
+            #top_p, top_class = prob.topk(1, dim = 1)
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
     print(f'Accuracy of the network on the {len(testdata)} testdata: {100 * correct // total}%')
-     #print(str(time.time() - tic) + ' s')
+    print(str(time.time() - tic) + ' s')
+    """
+    dataiter = iter(testloader)
+    inputs, labels = next(dataiter)
+    outputs = network(inputs)
+    prob = F.softmax(outputs, dim=1)
+    top_p, top_class = prob.topk(1, dim=1)
+   """
+   
+    
